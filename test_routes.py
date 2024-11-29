edf717dc49cff5e2c190e732c68694bdf342df90
Atul-728 <112749978+Atul-728@users.noreply.github.com>
2024-11-29 20:08:37 +0530

    Create test_routes.py

---
+++ test_routes.py
@@ -0,0 +1,20 @@
+def test_read_product_route():
+    # <your_code_here>
+
+def test_update_product_route():
+    # <your_code_here>
+
+def test_delete_product_route():
+    # <your_code_here>
+
+def test_list_all_products_route():
+    # <your_code_here>
+
+def test_list_by_name_route():
+    # <your_code_here>
+
+def test_list_by_category_route():
+    # <your_code_here>
+
+def test_list_by_availability_route():
+    # <your_code_here>
