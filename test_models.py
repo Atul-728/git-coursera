84b0d1faebfe952679aca90c959c6a8b07327fbe
Atul-728 <112749978+Atul-728@users.noreply.github.com>
2024-11-29 20:08:02 +0530

    Create test_models.py

---
+++ test_models.py
@@ -0,0 +1,20 @@
+def test_read_product():
+    # <your_code_here>
+
+def test_update_product():
+    # <your_code_here>
+
+def test_delete_product():
+    # <your_code_here>
+
+def test_list_all_products():
+    # <your_code_here>
+
+def test_find_by_name():
+    # <your_code_here>
+
+def test_find_by_category():
+    # <your_code_here>
+
+def test_find_by_availability():
+    # <your_code_here>
